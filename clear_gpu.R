04e0bb312265368660755e851870ba68125cc86c
Diabb Zegpi <42278160+DiabbZegpi@users.noreply.github.com>
2021-12-31 19:51:52 -0300

    Update clear_gpu v1.1

--- clear_gpu.R
+++ clear_gpu.R
@@ -1,3 +1,6 @@
+# The original code was adapted form https://github.com/rstudio/keras/issues/963
+# A simpler solution may exist, using numba.cuda, like this https://stackoverflow.com/questions/43930871
+
 library(reticulate)
 py_run_string('from keras import backend as K')
 py_run_string('from keras.backend import set_session;')
@@ -10,5 +13,3 @@ clear_gpu <- function() {
   py_run_string('K.set_session(K.tf.compat.v1.Session(config=cfg))')
   k_clear_session()
 }
-
-clear_gpu()
